a84a698dda92a2848a3946c5549ffa9fca9a624d
Michael Pettitt <mikejohnpettitt@gmail.com>
2025-12-03 15:25:50 +0100

    added pass button

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,21 +1,20 @@
 class UserSessionsController < ApplicationController
 
+  
   def show
     @user_session = current_or_guest_user.user_sessions.find(params[:id])
-
-    Song.where(playlist: @playlist).each do |song|
-      Question.create(session_id: @user_session.session_id, song: song )
-    end
-
-    raise
-    
-    if params[:current_question]
-      @next_question = Question.where(session_id: @user_session.session_id).where("id > ?", params[:current_question]).first
-      @question = @next_question
+    @playlist = @user_session.session.playlist
+    if params[:current_question].present?
+      @current_question = Question.find(params[:current_question])
+      @next_question = Question.where(session_id: @user_session.session_id).where("id > ?", @current_question).first
+      @current_question = @next_question
     else
-      @question = Question.where(session_id: @user_session.session_id).first
+      Song.where(playlist_id: @playlist.id).each do |song|
+        Question.create(session_id: @user_session.session_id, song: song )
+      end
+      @current_question = Question.where(session_id: @user_session.session_id).first
     end
-    
   end
+
 
 end
