455a457851a1e523b7fec8854d350219508928c7
Ludo de Bergeyck <ludo.bergeyck@gmail.com>
2025-12-26 18:24:16 +0200

    creation d'une playlist personnalisé mais on ne sait pas encore ajouter des song

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,6 @@
 class PlaylistsController < ApplicationController
+
+  before_action :authenticate_user!
   def index
     @playlists = Playlist.all
   end
@@ -6,4 +8,37 @@ class PlaylistsController < ApplicationController
   def user
     @user = current_user
   end
+
+def show 
+  @playlist = Playlist.find(params[:id])
 end
+
+def new 
+  @playlist = Playlist.new
+end
+
+def create 
+  @playlist = Playlist.new(playlist_params)
+  @playlist.user = current_user
+  if @playlist.save
+    redirect_to @playlist, notice: "Playlist created!"
+  else 
+    render :new
+  end
+end 
+
+ def destroy
+  @playlist = Playlist.find(params[:id])
+  @playlist.destroy
+    redirect_to playlists_path, notice: "Playlist deleted!"
+ end
+
+def my_playlists
+  @playlists = current_user.playlists
+end 
+
+ private 
+  def playlist_params
+  params.require(:playlist).permit(:title)
+  end
+end 
---
+++ test/controllers/songs_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class SongsControllerTest < ActionDispatch::IntegrationTest
+  test "should get search" do
+    get songs_search_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get songs_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get songs_destroy_url
+    assert_response :success
+  end
+end
